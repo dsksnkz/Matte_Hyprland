e9c9a9d2c992ec0eb0ab473d8c596c921f42b086
matte <duskbladez83@gmail.com>
2026-06-03 19:26:01 +0100

    Simplify installation commands in install.bash
    Replaced multiple commands with a single line for installation.

--- install.bash
+++ install.bash
@@ -1,8 +1,2 @@
-sudo
-cd .config
-
-git clone https://github.com/dsksnkz/my-hyprland-rice.git
-
-pacman -S cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix 
-
-exit
+# For NORMAL arch-hyprland dots
+sudo cd .config && git clone https://github.com/dsksnkz/my-hyprland-rice.git && pacman -S cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix && echo Hyprland-Default - Installation either completed or failed. You may now close this Window or report a bug. 
