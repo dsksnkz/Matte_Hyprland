abc4bb8dfffb3c48360609194ea0c25477324fe9
matte <duskbladez83@gmail.com>
2026-06-07 10:30:43 +0000

    Delete dotfiles/installNIRIWM.bash

--- dotfiles/installNIRIWM.bash
+++
@@ -1,2 +0,0 @@
-# For installing NIRI WM external (others are still normal)
-sudo cd .config && git clone https://github.com/dsksnkz/matte-Dotfiles.git && pacman -S niri-git cava rofi waybar fastfetch steam wlogout firefox nmtui helium obsidian blender logseq fish hyprlock hyprland-plugin-csgo-vulkan-fix && echo NIRI - Installation either completed or failed. You may now close this Window or report a bug. 
